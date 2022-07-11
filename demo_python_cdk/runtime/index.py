f0eb5da8213a4274cd0bc648cde02d58e10651cc
Sainath Machunuru <machunursainathgoud@gmail.com>
2022-07-12 00:32:45 +0530

    Added lambda logic to create a product

--- demo_python_cdk/runtime/index.py
+++ demo_python_cdk/runtime/index.py
@@ -1,2 +1,22 @@
+import boto3
+import json
+
 def handler(event, _context):
-    return {"body":"Hello from lambda", "statusCode": 200}
+    request_body = json.loads(event['body'])
+    product_code = request_body['product_code']
+    name = request_body['name']
+    if product_code == None or name == None:
+             return {"body":"product_code and name are required", "statusCode": 400}
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('products')
+        table.put_item(
+                Item = {
+                    'product_code': product_code,
+                    'name': name,
+                }
+            )
+        return {"body":"Product sucessfully created", "statusCode": 200}
+    except:
+        return {"body": "Internal server error", "statusCode": 500}
+    
